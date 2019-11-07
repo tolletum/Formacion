d77ace038a0aee222011e7f3969158db71208aa3
Alberto Barrajón <tolletum@gmail.com>
2019-11-07 23:16:57 +0100

    feat: section 11 incompleted task

---
+++ vue-js-2-the-complete-guide/tarea-11/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <form v-if="!isSubmitted">
+      <div class="row">
+        <!-- Exercise 1 -->
+        <!-- Create a Signup Form where you retrieve the following Information -->
+        <!-- Full Name (First Name + Last Name) -->
+        <!-- Mail -->
+        <!-- Password -->
+        <!-- Store Data? Yes/No -->
+
+        <!-- Exercise 2 -->
+        <!-- Only display the Form if it has NOT been submitted -->
+        <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+        <!-- Exercise 3 -->
+        <!-- Edit the Example from above and create a custom "Full Name" Control -->
+        <!-- which still holds the First Name and Last Name Input Field -->
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>Sign up</h1>
+          <hr />
+          <div class="form-group">
+            <label for="Full Name">Full Name</label>
+            <app-full-name v-model="fullName"></app-full-name>
+          </div>
+          <div class="form-group">
+            <label for="email">Mail</label>
+            <input type="text" id="email" class="form-control" v-model="email" />
+          </div>
+          <div class="form-group">
+            <label for="email">Password</label>
+            <input type="password" id="password" class="form-control" v-model="password" />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+          <label for="storeData">Store Data?:</label>
+          <label for="storeDataYes">
+            <input type="radio" id="storeDataYes" value="Yes" v-model="storeData" /> Yes
+          </label>
+          <label for="storeDataNo">
+            <input type="radio" id="storeDataNo" value="No" v-model="storeData" /> No
+          </label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
+        </div>
+      </div>
+    </form>
+    <hr />
+    <div class="row" v-if="isSubmitted">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Full Name: {{ fullName }}</p>
+            <p>Mail: {{ email }}</p>
+            <p>Password: {{ password }}</p>
+            <p>Store in Database?: {{ storeData }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FullName from "./components/FullName.vue";
+export default {
+  data() {
+    return {
+      fullName: "",
+      email: "",
+      password: "",
+      storeData: "No",
+      isSubmitted: false
+    };
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = true;
+    }
+  },
+  components: {
+    appFullName: FullName
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-js-2-the-complete-guide/tarea-11/src/components/FullName.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" @input="nameModified(value)" id="fullName" class="form-control" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  methods: {
+    nameModified(fullName) {
+      console.log("Entro en el metodo para el evento: " + fullName);
+      this.$emit("input", fullName);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
