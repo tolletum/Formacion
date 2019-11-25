c6e936dfa1072b3e030fa8c49d0f3b2d2ab0e74d
Alberto Barrajón <tolletum@gmail.com>
2019-11-25 22:36:55 +0100

    feat(vue): request to coordinator

--- vue-js-2-the-complete-guide/section-15/src/main.js
+++ vue-js-2-the-complete-guide/section-15/src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import VueResource from 'vue-resource'
-import App from './App.vue'
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import App from './App.vue';
 
 Vue.use(VueResource);
+
+Vue.http.options.root = 'http://localhost:8081/coordinators';
+Vue.http.headers.common['Access-Control-Allow-Origin'] = '*';
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
